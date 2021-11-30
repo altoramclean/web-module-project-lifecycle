7c8d3bd3194f1bd0a71207bb93095ba64f73f687
Altora Mclean <altoramclean@gmail.com>
2021-11-30 16:29:34 -0500

    going to work through the followers now

--- src/App.js
+++ src/App.js
@@ -1,11 +1,72 @@
 import React from 'react';
+import axios from 'axios';
+import User from './components/User';
+import FollowerList from './components/FollowerList';
 
 class App extends React.Component {
-  render() {
-    return(<div>
-      Github Card
-    </div>);
+  state = {
+    user: '',
+    followers: [],
+    input: userName
+  }
+
+  handleChange = evt => {
+    this.setState({
+      ...this.state,
+      input: evt.target.value
+    })
+  }
+
+  handleSubmit = evt => {
+    evt.preventDefault();
+    axios.get(`https://api.github.com/users/${this.state.input}`)
+      .then(resp => {
+        this.setState({
+          ...this.state,
+          user: resp.data
+        })
+      })
+  }
+componentDidMount(){
+  axios.get('https://api.github.com/users/altoramclean')
+  .then(resp => {
+    this.setState({
+      ...this.state,
+      user: resp.data
+    })
+  })
+  .catch(err => {
+    console.error(err)
+  })
+}
+render(){
+  return (
+    <div>
+      <h1>Github</h1>
+      <div className="search-bar">
+            <form onSubmit={this.handleSubmit}>
+                <input
+                  type = 'text'
+                  value = {this.state.input}
+                  onChange = {this.handleChange}
+                  placeholder = 'Enter Github '
+                />
+                <button>Search</button>
+            </form>
+
+            <User user={this.state.user}/>
+    </div>
+  )
+}
+<FollowerList followers={this.state.followers}/>
+        </div>
+  )
   }
 }
+
+
+
+
+
 
 export default App;
